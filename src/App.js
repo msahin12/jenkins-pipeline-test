c6826e0cf4c40006033ae50d2011797179f586bb
Mustafa SAHIN <gakeko2018@gmail.com>
2020-04-26 15:08:11 +0300

    9th commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload8.
+          Edit <code>src/App.js</code> and save to reload9.
         </p>
         <a
           className="App-link"
